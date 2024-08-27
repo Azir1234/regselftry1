4056566e1fe0618d44e4520570e637cd071522a7
18379924683 <scm1234567899@163.com>
2024-08-27 18:24:55 +0800

    +静态资源映射

---
+++ src/main/java/com/azir/RegselftryApplication.java
@@ -0,0 +1,12 @@
+package com.azir;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegselftryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegselftryApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/azir/configration/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.azir.configration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Configurable;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+@Slf4j
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        super.addResourceHandlers(registry);
+    }
+}
